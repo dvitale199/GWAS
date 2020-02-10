2f8638b03764336464c158395770d8578c442ec4
dvitale199 <d.vitale199@gmail.com>
2020-02-10 17:46:00 -0500

    meta_analysis_data_prep added plink format

--- scripts/.ipynb_checkpoints/meta_analysis_data_prep-checkpoint.py
+++ scripts/.ipynb_checkpoints/meta_analysis_data_prep-checkpoint.py
@@ -3,30 +3,49 @@ import os
 import re
 from functools import reduce
 
-file_list = os.listdir('/data/vitaled2/summary_stats')
+file_list = os.listdir('/data/vitaled2/summary_stats/rvtest_files/')
 suffixes = [re.search(r'\.(.*?)\.', cohort).group(1) for cohort in file_list]
 
-summary_data_list = [pd.read_csv('/data/vitaled2/summary_stats/' + file, sep='\t') for file in file_list]
+summary_data_list = [pd.read_csv('/data/vitaled2/summary_stats/rvtest_files/' + file, sep='\t') for file in file_list]
 
-for i in range(len(summary_data_list)):
-    summary_data_list[i]['cohort'] = suffixes[i]
+# for i in range(len(summary_data_list)):
+#     df = summary_data_list[i]
+#     df2 = df[['markerID', 'beta', 'se', 'P']]
+#     df2.columns = ['SNP','OR','SE','P']
+#     split = df.markerID.str.split(':', expand=True)
+#     df2['CHR'] = split[0]
+#     df2['BP'] = split[1]
+#     df2['A1'] = df.minorAllele
+#     df2['A2'] = df.majorAllele
+#     df_final = df2.sort_values('P').drop_duplicates(['SNP'], keep='first')
+#     df_final.to_csv('/data/vitaled2/summary_stats/' + suffixes[i] + '_random_effect_data.assoc', sep='\t', index=False)
 
-summary_df = pd.concat(summary_data_list)
 
-# drop duplicate markers within cohorts, keeping maker with lowest pval
-summary_df_cleaned = summary_df.sort_values('P').drop_duplicates(['markerID','cohort'], keep='first')
+#     plink2 --meta-analysis COURAGE_UK_random_effect_data.assoc HBS_random_effect_data.assoc IPDGC_random_effect_data.assoc PDBP_random_effect_data.assoc PPMI_random_effect_data.assoc
 
 
+############This method combines all into a single df for R 'meta' implementation
+# for i in range(len(summary_data_list)):
+#     summary_data_list[i]['cohort'] = suffixes[i]
 
-# markers_list = []
-# for i in range(len(file_list)):
-#     markers = pd.read_csv('summary_stats/' + file_list[i], sep='\t', usecols=['markerID'])
-#     markers_list.append(summary)
+# summary_df = pd.concat(summary_data_list)
+
+# # # drop duplicate markers within cohorts, keeping maker with lowest pval
+# summary_df_cleaned = summary_df.sort_values('P').drop_duplicates(['markerID','cohort'], keep='first')
+
+
+
+markers_list = []
+for i in range(len(file_list)):
+    markers = pd.read_csv('/data/vitaled2/summary_stats/rvtest_files/' + file_list[i], sep='\t', usecols=['markerID'])
+    markers_list.append(summary)
 
 # select markers that exist across all datasets
 merged = reduce(lambda x, y: pd.merge(x, y, on=['markerID'], how='inner'), summary_data_list) 
 marker_list = list(merged.drop(merged.columns.difference(['markerID']), axis=1).drop_duplicates('markerID').markerID)
 
-final_df = summary_df_cleaned[summary_df_cleaned.markerID.isin(marker_list)]
-final_sorted = final_df.sort_values(['markerID','cohort'])
+# final_df = summary_df_cleaned[summary_df_cleaned.markerID.isin(marker_list)]
+# final_sorted = final_df.sort_values(['markerID','cohort'])
+# final = final_sorted[['markerID', 'cohort', 'beta', 'se']]
 
+# final.to_csv("random_effect_test_5_cohorts.csv", index=False, sep='\t')
--- scripts/meta_analysis_data_prep.py
+++ scripts/meta_analysis_data_prep.py
@@ -3,32 +3,60 @@ import os
 import re
 from functools import reduce
 
-file_list = os.listdir('/data/vitaled2/summary_stats')
+file_list = os.listdir('/data/vitaled2/summary_stats/rvtest_files/')
 suffixes = [re.search(r'\.(.*?)\.', cohort).group(1) for cohort in file_list]
 
-summary_data_list = [pd.read_csv('/data/vitaled2/summary_stats/' + file, sep='\t') for file in file_list]
 
-for i in range(len(summary_data_list)):
-    summary_data_list[i]['cohort'] = suffixes[i]
 
-summary_df = pd.concat(summary_data_list)
+# the following generates .assoc files for plink --meta-analysis method 
+# summary_data_list = [pd.read_csv('/data/vitaled2/summary_stats/rvtest_files/' + file, sep='\t') for file in file_list]
+# for i in range(len(summary_data_list)):
+#     df = summary_data_list[i]
+#     df2 = df[['markerID', 'beta', 'se', 'P']]
+#     df2.columns = ['SNP','OR','SE','P']
+#     split = df.markerID.str.split(':', expand=True)
+#     df2['CHR'] = split[0]
+#     df2['BP'] = split[1]
+#     df2['A1'] = df.minorAllele
+#     df2['A2'] = df.majorAllele
+#     df_final = df2.sort_values('P').drop_duplicates(['SNP'], keep='first')
+#     df_final.to_csv('/data/vitaled2/summary_stats/' + suffixes[i] + '_random_effect_data.assoc', sep='\t', index=False)
 
-# drop duplicate markers within cohorts, keeping maker with lowest pval
-summary_df_cleaned = summary_df.sort_values('P').drop_duplicates(['markerID','cohort'], keep='first')
+
+#     plink2 --meta-analysis COURAGE_UK_random_effect_data.assoc HBS_random_effect_data.assoc IPDGC_random_effect_data.assoc PDBP_random_effect_data.assoc PPMI_random_effect_data.assoc
+
+
+############This method combines all into a single df for R 'meta' implementation
+# for i in range(len(summary_data_list)):
+#     summary_data_list[i]['cohort'] = suffixes[i]
+
+# summary_df = pd.concat(summary_data_list)
+
+# # # drop duplicate markers within cohorts, keeping maker with lowest pval
+# summary_df_cleaned = summary_df.sort_values('P').drop_duplicates(['markerID','cohort'], keep='first')
 
 
 
 # markers_list = []
 # for i in range(len(file_list)):
-#     markers = pd.read_csv('summary_stats/' + file_list[i], sep='\t', usecols=['markerID'])
-#     markers_list.append(summary)
+#     markers = pd.read_csv('/data/vitaled2/summary_stats/rvtest_files/' + file_list[i], sep='\t', usecols=['markerID', 'beta', 'se'])
+#     markers_list.append(markers)
+
+summary_data_list = [pd.read_csv('/data/vitaled2/summary_stats/rvtest_files/' + file, sep='\t', usecols=['markerID','beta','se','P']) for file in file_list]
+
+summary_data_list_cleaned = []
+for i in range(len(summary_data_list)):
+    summary_data_list[i].sort_values('P').drop_duplicates(['markerID'], keep='first')
 
 # select markers that exist across all datasets
 merged = reduce(lambda x, y: pd.merge(x, y, on=['markerID'], how='inner'), summary_data_list) 
-marker_list = list(merged.drop(merged.columns.difference(['markerID']), axis=1).drop_duplicates('markerID').markerID)
+merged.to_csv("random_effect_test_5_cohorts.txt", index=False, header=False)
 
-final_df = summary_df_cleaned[summary_df_cleaned.markerID.isin(marker_list)]
-final_sorted = final_df.sort_values(['markerID','cohort'])
-final = final_sorted[['markerID', 'cohort', 'beta', 'se']]
 
-final.to_csv("random_effect_test_5_cohorts.csv", index=False, sep='\t')
+# marker_list = list(merged.drop(merged.columns.difference(['markerID']), axis=1).drop_duplicates('markerID').markerID)
+
+# final_df = summary_df_cleaned[summary_df_cleaned.markerID.isin(marker_list)]
+# final_sorted = final_df.sort_values(['markerID'])
+# final = final_sorted[['markerID', 'beta', 'se']]
+
+# final.to_csv("random_effect_test_5_cohorts.csv", index=False, sep='\t')
