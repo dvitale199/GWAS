f305c9c801862ef908fa955a62ecab4c378e1928
dvitale199 <d.vitale199@gmail.com>
2020-02-12 17:49:39 -0500

    added meta_analysis_data_prep.py to GWAS run script

--- scripts/meta_analysis_data_prep.py
+++ scripts/meta_analysis_data_prep.py
@@ -7,40 +7,49 @@ import subprocess
 import argparse
 
 # convert rvtest .tab files ---> PLINK .assoc files and rvtest .tab files ---> metasoft .txt files
-parser = argparse.ArgumentParser(description='Arguments for GWAS Meta-analysis
+parser = argparse.ArgumentParser(description='Arguments for GWAS Meta-analysis')
 # parser.add_argument('--assoc', type=str, default='nope', help='GWAS: (string file path). Path to PLINK format .assoc files, full path as follows: /path/to/files/dir/ [default: nope].')
-parser.add_argument('--tab', type=str, default='nope', help='GWAS: (string file path). Path to rvtest format .tab files, everything before the ".assoc" [default: nope].')
-parser.add_argument('--out_plink', type=str, default='out', help='Prefix for plink .assoc output (including path)')
-parser.add_argument('--out_metasoft', type=str, default='out', help='Prefix for metasoft .txt output (including path)')
-                                 
+parser.add_argument('--tab', type=str, default='nope', help='GWAS: (string file path). Path to rvtest format .tab files, /dir/containing/files/ [default: nope].')
+parser.add_argument('--out_plink', type=str, default='nope', help='Prefix for plink .assoc output (including path)')
+parser.add_argument('--out_metasoft', type=str, default='nope', help='Prefix for metasoft .txt output (including path)')
+                                                   
 args = parser.parse_args()
 
+# basedir = '/data/vitaled2/summary_stats/rvtest_files/'
 
-
-basedir = '/data/vitaled2/summary_stats/rvtest_files/'
-file_list = glob.glob(basedir + '*.tab')
-suffixes = [re.search(r'\.(.*?)\.', cohort).group(1) for cohort in file_list]
-
-
-
-# the following generates .assoc files for plink --meta-analysis method from rvtest .tab files
-def plink_meta_parse(assoc_path, out_path):
-    summary_data_list = [pd.read_csv('/data/vitaled2/summary_stats/rvtest_files/' + file, sep='\t') for file in file_list]
+def plink_meta_parse(tab_path, out_path):
+    """
+    generates .assoc files for plink --meta-analysis method from rvtest .tab files
+    """
+    print("Parsing...")
+    print()
+    file_list = glob.glob(tab_path + '*.tab')
+    suffixes = [re.search(r'\.(.*?)\.', cohort).group(1) for cohort in file_list]
+    summary_data_list = [pd.read_csv(file, sep='\t') for file in file_list]
     for i in range(len(summary_data_list)):
         df = summary_data_list[i]
-        df2 = df[['markerID', 'beta', 'se', 'P']]
+        df2 = df[['markerID', 'beta', 'se', 'P']].copy()
         df2.columns = ['SNP','OR','SE','P']
         split = df.markerID.str.split(':', expand=True)
-        df2['CHR'] = split[0]
-        df2['BP'] = split[1]
-        df2['A1'] = df.minorAllele
-        df2['A2'] = df.majorAllele
-        df_final = df2.sort_values('P').drop_duplicates(['SNP'], keep='first')
-        df_final.to_csv('/data/vitaled2/summary_stats/' + suffixes[i] + '_random_effect_data.assoc', sep='\t', index=False)
+        split.columns = ['CHR','BP']
+        df2.loc[:,'CHR'] = split.CHR
+        df2.loc[:,'BP'] = split.BP
+        df2.loc[:,'A1'] = df.minorAllele
+        df2.loc[:,'A2'] = df.majorAllele
+        df_final = df2.sort_values('P').drop_duplicates(['SNP'], keep='first').copy()
+        df_final.to_csv(out_path + suffixes[i] + '_random_effect_data.assoc', sep='\t', index=False)
+        print("PLINK .assoc file written to:")
+        print(out_path + suffixes[i] + '_random_effect_data.assoc')
+    print("DONE!")
 
 
-# the following generates .txt files with "snp" "beta1" "se1" "beta2" "se2"... "betaN" "seN" for N number of GWAS inputs for metasoft java meta analysis implementation from rvtests .tab files
-def metasoft_meta_parse(in_path, out_path):
+def metasoft_meta_parse(tab_path, out_path):
+    """
+    generates .txt files with "snp" "beta1" "se1" "beta2" "se2"... "betaN" "seN" for N number of GWAS inputs for metasoft java meta analysis implementation from rvtests .tab files"""
+    
+    print("Parsing...")
+    print()
+    file_list = glob.glob(tab_path + '*.tab')
     summary_data_list_cleaned = []
     for i in range(len(file_list)):
         df = pd.read_csv(file_list[i], sep='\t', usecols=['markerID','beta','se','P'])
@@ -50,13 +59,39 @@ def metasoft_meta_parse(in_path, out_path):
 
     # select markers that exist across all datasets
     merged = reduce(lambda x, y: pd.merge(x, y, on=['markerID'], how='inner'), summary_data_list_cleaned) 
-    merged.to_csv(basedir + "random_effect_test_5_cohorts.txt", index=False, header=False, sep='\t')
+    merged.to_csv(out_path + 'random_effect_model_' + str(len(file_list)) + '_cohorts.txt', index=False, header=False, sep='\t')
+    print("Metasoft .txt file written to:")
+    print(out_path + 'random_effect_model_' + str(len(file_list)) + '_cohorts.txt')
+        
+in_tab = args.tab
+out_plink = args.out_plink
+out_metasoft = args.out_metasoft
 
+if (in_tab == "nope"):
+    print("looks like you don't have any GWAS files to parse! get some GWAS data from rvtests and try again!")
 
     
+if (in_tab != "nope") & (out_plink != "nope"):
+    print("NOW PARSING: ")
+    for file in glob.glob(in_tab + '*.tab'):
+        print(file)
+    print("TO PLINK .assoc FORMAT")
+    print()
     
+    plink_meta_parse(in_tab, out_plink)
     
+if (in_tab != "nope") & (out_metasoft != "nope"):
+    print("NOW PARSING: ")
+    for file in glob.glob(in_tab + '*.tab'):
+        print(file)
+    print("TO METASOFT .txt FORMAT")
+    print()
     
+    metasoft_meta_parse(in_tab, out_metasoft)  
+                                
+
+        
+###### UNUSED SHIT ######
     
 #     plink2 --meta-analysis COURAGE_UK_random_effect_data.assoc HBS_random_effect_data.assoc IPDGC_random_effect_data.assoc PDBP_random_effect_data.assoc PPMI_random_effect_data.assoc
 
