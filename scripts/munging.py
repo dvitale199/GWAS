57db39ca7c693644bdb67f26a05208c59e577b51
dvitale199 <d.vitale199@gmail.com>
2020-02-25 17:20:40 -0500

    added conditionals to QC run

--- scripts/munging.py
+++ scripts/munging.py
@@ -1,6 +1,7 @@
 import subprocess
 import argparse
 import pandas as pd
+import os
 
 parser = argparse.ArgumentParser(description='Arguments for Genotyping QC (data in Plink .bim/.bam/.fam format)')
 parser.add_argument('--geno', type=str, default='nope', help='Genotype: (string file path). Path to PLINK format genotype file, everything before the *.bed/bim/fam [default: nope].')
@@ -8,17 +9,18 @@ parser.add_argument('--out', type=str, default='out', help='Prefix for output (i
 
 args = parser.parse_args()
 
-print("GENOTYPE FILES", args.geno)
+print("PROCESSING THE FOLLOWING GENOTYPES:", args.geno)
 
 def logging(geno_path, out_path):
     out_name = geno_path + "_GWAS_QC.log"
-    log = open(out_name, "w", newline='\n')
+    log = open(out_name, "a", newline='\n')
     
     return log
     
 #QC and data cleaning
 def het_pruning(geno_path, out_path):
-    log = logging(geno_path, out_path)
+    print("NOW PRUNING FOR HETEROZYGOSITY")
+    
     bash1 = "plink --bfile " + geno_path + " --geno 0.01 --maf 0.05 --indep-pairwise 50 5 0.5 --out " + out_path + "pruning"
     bash2 = "plink --bfile " + geno_path + " --extract " + out_path + "pruning.prune.in --make-bed --out " + out_path + "pruned_data"
     bash3 = "plink --bfile " + out_path + "pruned_data --het --out " + out_path + "prunedHet"
@@ -32,6 +34,7 @@ def het_pruning(geno_path, out_path):
     
     cmds = [bash1, bash2, bash3, bash4, bash5, bash6, bash7]
     
+    log = logging(geno_path, out_path)
     log.write("RUNNING QC FOR " + geno_path)
     log.write("\n")
     log.write("PRUNING FOR HETEROZYGOSITY WITH THE FOLLOWING COMMANDS:")
@@ -49,19 +52,35 @@ def het_pruning(geno_path, out_path):
     log.close()
         
         
-
 def call_rate_pruning(geno_path, out_path):
+    print("NOW PRUNING FOR CALL RATE")
     
     bash1 = "plink --bfile " + geno_path + " --mind 0.05 --make-bed --out " + geno_path + "_call_rate"
     bash2 = "mv " + geno_path + "_after_call_rate.irem " + out_path + "CALL_RATE_OUTLIERS.txt"
     
     cmds = [bash1, bash2]
+    
+    log = logging(geno_path, out_path)
+    log.write("\n")
+    log.write("PRUNING FOR CALL RATE WITH THE FOLLOWING COMMANDS:")
+    log.write("\n")
+    log.write("\n")
+    
     for cmd in cmds:
+        log.write(cmd)
+        log.write("\n")
         subprocess.run(cmd, shell=True)
+        
+    log.write("***********************************************")
+    log.write("***********************************************")
+    log.write("***********************************************")
+    log.close()    
+        
 
 def sex_check(geno_path, out_path):
+    print("NOW CHECKING SEXES")
     bash1 = "plink --bfile " + geno_path + " --check-sex 0.25 0.75 --maf 0.05 --out " + out_path + "gender_check1"
-    bash2 = "plink --bfile "+ geno_path + "--chr 23 --from-bp 2699520 --to-bp 154931043 --maf 0.05 --geno 0.05 --hwe 1E-5 --check-sex  0.25 0.75 --out " + out_path + "gender_check2"
+    bash2 = "plink --bfile "+ geno_path + " --chr 23 --from-bp 2699520 --to-bp 154931043 --maf 0.05 --geno 0.05 --hwe 1E-5 --check-sex  0.25 0.75 --out " + out_path + "gender_check2"
     bash3 = "grep 'PROBLEM' " + out_path + "gender_check1.sexcheck > " + out_path + "problems1.txt"
     bash4 = "grep 'PROBLEM' " + out_path + "gender_check2.sexcheck > " + out_path + "problems2.txt"
     bash5 = "cat " + out_path + "problems1.txt " + out_path + "problems2.txt > " + out_path + "GENDER_FAILURES.txt"
@@ -70,10 +89,29 @@ def sex_check(geno_path, out_path):
     
     cmds = [bash1, bash2, bash3, bash4, bash5, bash6, bash7]
     
+    log = logging(geno_path, out_path)
+    log.write("\n")
+    log.write("PRUNING FOR SEX WITH THE FOLLOWING COMMANDS:")
+    log.write("\n")
+    log.write("\n")
+    
     for cmd in cmds:
+        log.write(cmd)
+        log.write("\n")
         subprocess.run(cmd, shell=True)
+        
+    log.write("***********************************************")
+    log.write("***********************************************")
+    log.write("***********************************************")
+    log.close()
+ 
 
- ###########################################################################################################################################################################################################################################################################################################################################################################################################################################################################
+
+
+
+
+
+###########################################################################################################################################################################################################################################################################################################################################################################################################################################################################
 # MAY NEED TO ADD FUNCTION FOR ANCESTRY OUTLIERS (PCR FOR RELATEDNESS)     
         
         
@@ -126,10 +164,25 @@ geno_relatedness = geno_sex + "_relatedness"
 geno_variant = geno_relatedness + "_variant"
 
 
-############### need to write if/else statement for if new file names arent generated!!!!!!!!!!!!!!!!!! ###############
+
+# het pruning
 het_pruning(geno, out)
-# call_rate_pruning(geno_het, out)
-# sex_check(geno_call_rate, out)
+
+# check if geno_het output exists, if not, run on original geno data (there may not have been anything pruned in previous step and thus, no new file created)
+if os.path.exists(geno_het + ".bim"):
+    call_rate_pruning(geno_het, out)
+else:
+    call_rate_pruning(geno, out)
+
+# check if geno_call_rate exists, if not, check geno_het, if not, use original
+if os.path.exists(geno_call_rate + ".bim"):
+    sex_check(geno_call_rate, out)
+elif os.path.exists(geno_het + ".bim"):
+    sex_check(geno_het, out)
+else:
+    sex_check(geno, out)
+
+
 # relatedness_pruning(geno_sex, out)
 # variant_pruning(geno_relatedness, out)
 
