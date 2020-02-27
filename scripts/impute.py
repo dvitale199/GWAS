01f9cad8b3ceb6d5fd2443bf2d530e3a5169fe5b
dvitale199 <d.vitale199@gmail.com>
2020-02-27 17:45:23 -0500

    began creating data prep function in impute.py

--- scripts/impute.py
+++ scripts/impute.py
@@ -12,3 +12,40 @@ args = parser.parse_args()
 geno = args.geno
 out = args.out
 
+
+
+
+################################################
+#DATA PREP
+
+def prep_data(geno_path, out_path):
+    # download file to check
+    bash1 = "wget " + "http://www.well.ox.ac.uk/~wrayner/tools/HRC-1000G-check-bim.v4.2.5.zip -P " + out_path
+     
+    bash2 = "unzip " + out_path + "HRC-1000G-check-bim.v4.2.5.zip"
+    
+    # make your .frq file
+    bash3 = "plink --bfile " + geno_path " --freq --out " geno_path
+
+    bash4 = "perl " + out_path + HRC-1000G-check-bim.pl -b $FILENAME.bim -f $FILENAME.frq -r HRC.r1-1.GRCh37.wgs.mac5.sites.tab -h
+
+# then run to fix your data
+sh Run-plink.sh
+
+# then make vcf files
+
+for chnum in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+  do
+  plink --bfile YOURFILE-updated-chr$chnum --recode vcf --chr $chnum --out YOURFILE$chnum 
+done
+
+## then sort and zip
+
+module load vcftools
+
+for chnum in {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+  do
+	vcf-sort YOURFILE$chnum.vcf | bgzip -c >  pre_impute_YOURFILE_$chnum.vcf.gz
+done
+
+# and then you are ready to submit to the imputation server
