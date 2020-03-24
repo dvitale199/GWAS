20e486f69cb7d30c3e9b91993582b2921274d240
dvitale199 <d.vitale199@gmail.com>
2020-03-24 16:53:31 -0400

    created plink_helper package and moved logging and driver in to plink_logging.py and plink_driver.py and put commands into qc_steps.py

---
+++ plink_helper/.ipynb_checkpoints/plink_logger-checkpoint.py
@@ -0,0 +1,31 @@
+import os
+
+class Logger:
+    def __init__(self, geno_path, out_path):
+        self.geno_path = geno_path
+        self.out_path = out_path
+        print("PROCESSING THE FOLLOWING GENOTYPES:", geno_path)
+        
+        
+    def logging(self):
+        log_out_name = self.geno_path + ".PLINK_STEPS.log"
+        print("logging to " + log_out_name)
+        log = open(log_out_name, "a", newline='\n')
+        log.write("***********************************************")
+        log.write("\n")
+        log.write("***********************************************")
+        log.write("\n")
+        log.write("RUNNING PLINK COMMANDS FOR " + self.geno_path)
+        log.write("\n")
+        log.write("***********************************************")
+        log.write("\n")
+        log.write("***********************************************")
+        log.write("\n")
+            
+        return log
+    
+    def rm_log(self):
+        log_out_name = self.geno_path + ".PLINK_STEPS.log"
+        if os.path.exists(log_out_name):
+            os.remove(log_out_name)
+        
